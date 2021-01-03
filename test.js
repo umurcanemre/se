7e88ff3abf0e4c6a84a226ef62b9838d2a9d038b
umurcanemre <ibrahimumurcanemre@hotmail.com>
2021-01-03 22:50:33 +0100

    Initial commit. Sidebar loading moved to js file to avour duplication

---
+++ test.js
@@ -0,0 +1,88 @@
+var sidebar = '<div class="inner">\
+<!-- Search -->\
+<section id="search" class="alt">\
+    <form method="post" action="#">\
+        <input type="text" name="query" id="query" placeholder="Search" />\
+    </form>\
+</section>\
+<!-- Menu -->\
+<nav id="menu">\
+    <header class="major">\
+        <h2>Menu</h2>\
+    </header>\
+    <ul>\
+        <li><a href="index.html">Anasayfa</a></li>\
+        <li><a href="about.html">Hakkında</a></li>\
+        <li><a href="office.html">Muhayenehanemiz</a></li>\
+        <li>\
+            <span class="opener">Hizmetler</span>\
+            <ul>\
+                <li><a href="#">Sünnet</a></li>\
+                <li><a href="#">Muhayene</a></li>\
+            </ul>\
+        </li>\
+        <!--<li><a href="#">Etiam Dolore</a></li>\
+                        <li><a href="#">Adipiscing</a></li>\
+                        <li>\
+                            <span class="opener">Another Submenu</span>\
+                            <ul>\
+                                <li><a href="#">Lorem Dolor</a></li>\
+                                <li><a href="#">Ipsum Adipiscing</a></li>\
+                                <li><a href="#">Tempus Magna</a></li>\
+                                <li><a href="#">Feugiat Veroeros</a></li>\
+                            </ul>\
+                        </li>\
+                        <li><a href="#">Maximus Erat</a></li>\
+                        <li><a href="#">Sapien Mauris</a></li>\
+                        <li><a href="#">Amet Lacinia</a></li>-->\
+    </ul>\
+</nav>\
+<!-- Section\
+                <section>\
+                    <header class="major">\
+                        <h2>Ante interdum</h2>\
+                    </header>\
+                    <div class="mini-posts">\
+                        <article>\
+                            <a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>\
+                            <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>\
+                        </article>\
+                        <article>\
+                            <a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>\
+                            <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>\
+                        </article>\
+                        <article>\
+                            <a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>\
+                            <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>\
+                        </article>\
+                    </div>\
+                    <ul class="actions">\
+                        <li><a href="#" class="button">More</a></li>\
+                    </ul>\
+                </section> -->\
+<!-- Section -->\
+<section>\
+    <header class="major">\
+        <h2>İletişim</h2>\
+    </header>\
+    <p>Sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem\
+        ullamcorper dolore. Proin sed aliquam facilisis ante interdum. Sed nulla amet lorem feugiat\
+        tempus aliquam.</p>\
+    <ul class="contact">\
+        <li class="icon solid fa-envelope"><a\
+                href="mailto:drselahattinemre@gmail.com">drselahattinemre@gmail.com</a></li>\
+        <li class="icon solid fa-phone">(0537) 474 4939</li>\
+        <li class="icon solid fa-home">8in ora<br />\
+            Şehzadeler/Manisa</li>\
+    </ul>\
+</section>\
+<!-- Footer -->\
+<footer id="footer">\
+    <p class="copyright">&copy; Untitled. All rights reserved. Demo Images: <a\
+            href="https://unsplash.com">Unsplash</a>. Tasarım: <a href="https://html5up.net">HTML5\
+            UP</a>.</p>\
+</footer>\
+</div>';
+
+//document.getElementById("sidebar-filler").innerHTML='<object type="text/html" data="sidebar.html" ></object>';
+document.write(sidebar);
